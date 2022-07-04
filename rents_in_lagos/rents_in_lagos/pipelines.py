8a30b3c300208bbf5a81a6d3496773aff8782820
Fuadeiza <garbafuad@gmail.com>
2022-07-04 12:37:25 +0100

    Add result validation and improve  spider

--- rents_in_lagos/rents_in_lagos/pipelines.py
+++ rents_in_lagos/rents_in_lagos/pipelines.py
@@ -6,8 +6,19 @@
 
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
+from datetime import datetime
+
+from scrapy.exceptions import DropItem
 
 
 class RentsInLagosPipeline:
     def process_item(self, item, spider):
-        return item
+        required_item_fields = ["house_type", "price", "location"]
+        if all(field in item.keys() for field in required_item_fields):
+            item["crawled"] = datetime.now()
+            return item
+        else:
+            missing_fields = [
+                field for field in required_item_fields if field not in item.keys()
+            ]
+            raise DropItem(f"Missing item fields: {missing_fields}")
