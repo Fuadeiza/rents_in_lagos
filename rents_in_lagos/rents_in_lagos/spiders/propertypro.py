411539701aa6e867eda2008597bc07460973eaa7
Fuadeiza <garbafuad@gmail.com>
2022-07-06 18:15:00 +0100

    Update location to latitude and longitude

--- rents_in_lagos/rents_in_lagos/spiders/propertypro.py
+++ rents_in_lagos/rents_in_lagos/spiders/propertypro.py
@@ -1,60 +1,57 @@
 from types import NoneType
 import scrapy
 import re
+import ast
 
 from itemloaders.processors import MapCompose
 from scrapy.loader import ItemLoader
 
 from ..items import FileItem
 
-location_list = [
-    "Abule Egba",
-    "Agege",
-    "Ajah",
-    "Ajaokuta",
-    "Alimosho",
-    "Amuwo Odofin",
-    "Apapa",
-    "Arepo",
-    "Badagry",
-    "Bariga",
-    "Egbe Idimu",
-    "Egbeda",
-    "Ejigbo",
-    "Epe",
-    "Gbagada",
-    "Ibeju Lekki",
-    "Iju",
-    "Ikeja",
-    "Ikorodu",
-    "Ikotun Igando",
-    "Ikoyi",
-    "Ilaje",
-    "Ilupeju",
-    "Ipaja",
-    "Isolo",
-    "Ketu",
-    "Kosofe Ikosi",
-    "Lagos Island",
-    "Lekki",
-    "Maryland",
-    "Mushin",
-    "Ogba",
-    "Ogba Egbema Ndoni",
-    "Ogudu",
-    "Ojo",
-    "Ojodu",
-    "Ojota",
-    "Okota",
-    "Orile",
-    "Oshodi",
-    "Sangotedo",
-    "Shagari",
-    "Shomolu",
-    "Surulere",
-    "Victoria Island",
-    "Yaba",
-]
+geo_location_list = {
+    "Abule Egba": [6.619413,3.510454],
+    "Agege": [6.615356, 3.323782],
+    "Ajah": [6.466667,3.566667],
+    "Alimosho": [6.546609,3.238251],
+    "Amuwo Odofin" : [6.429173,3.288546],
+    "Apapa": [6.4499982, 3.3666652],
+    "Arepo": [6.465422,3.406448],
+    "Badagry": [6.416667,2.883333],
+    "Bariga": [6.533333,3.383333],
+    "Egbe Idimu": [6.453056,3.395833],
+    "Egbeda":[6.591947,3.289574],
+    "Epe":[6.583333,3.983333],
+    "Gbagada":[6.542983,3.392753],
+    "Ibeju Lekki": [6.496267,3.596457],
+    "Iju": [6.63656, 3.323811],
+    "Ikeja": [6.605874,3.349149],
+    "Ikorodu": [6.616865,3.508072],
+    "Ikotun Igando": [6.59618941675, 3.35544653886],
+    "Ikoyi": [6.454812,3.434691],
+    "Ilaje": [6.531267, 3.394722],
+    "Ilupeju": [6.553648,3.356674],
+    "Ipaja": [6.610044,3.289408],
+    "Isolo": [6.529962,3.331985],
+    "Ketu": [6.606967,3.381726],
+    "Kosofe Ikosi": [6.6074,3.38462],
+    "Lagos Island": [6.4499982, 3.3999984],
+    "Lekki": [6.458985, 3.601521],
+    "Maryland": [6.576421,3.365344],
+    "Mushin": [6.5333312, 3.3499986],
+    "Ogba": [6.632478,3.341067],
+    "Ogudu" : [6.580166,3.386174],
+    "Ojo": [6.4666648, 3.1833326],
+    "Ojodu": [6.625822, 3.354006],
+    "Ojota" : [6.578976,3.38542],
+    "Okota": [6.508835,3.313712],
+    "Orile": [6.630914,3.316181],
+    "Oshodi": [6.514193, 3.308678],
+    "Sangotedo": [6.453056,3.395833],
+    "Shomolu": [6.532954, 3.36739],
+    "Surulere": [6.500000, 3.350000],
+    "Victoria Island": [6.431111,3.415833],
+    "Yaba": [6.50837,3.384247]
+}
 
 
 class PropertyproSpider(scrapy.Spider):
@@ -80,9 +77,11 @@ class PropertyproSpider(scrapy.Spider):
             if location in UNWANTED_VALUES:
                 locations.remove(location)
         for value in locations:
-            for loc in location_list:
+            for loc in list(geo_location_list.keys()):
                 if loc in value:
-                    cleaned_location.append(value.replace(value,loc))
+                    location_str = value.replace(value,str(geo_location_list[loc]))
+                    cleaned_location.append(ast.literal_eval(location_str))
+
 
         for title in titles:
             item = ItemLoader(FileItem(), title)
@@ -91,11 +90,14 @@ class PropertyproSpider(scrapy.Spider):
                 cleaned_title = re.findall('[0-9]+', title_text)[0]
             except IndexError:
                 cleaned_title = 0
-            item.add_value("house_type", int(cleaned_title))
-            num = titles.index(title)
-            item.add_value("price", int(cleaned_price[num]))
-            item.add_value("location", cleaned_location[num])
-            yield item.load_item()
+            try:
+                item.add_value("house_type", int(cleaned_title))
+                num = titles.index(title)
+                item.add_value("price", int(cleaned_price[num]))
+                item.add_value("location", cleaned_location[num])
+                yield item.load_item()
+            except IndexError:
+                pass
         # next_page = response.css("a[alt='view next property page']::attr('href')").get()
         # if next_page is not None:
         #     yield response.follow(next_page, callback=self.parse)
